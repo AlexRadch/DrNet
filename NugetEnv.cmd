16ffaeeb381cb6280c2e19bf8100c698ba4f2d2a
Alexander Radchenko <radchenkosasha@gmail.com>
2021-09-03 01:28:37 +0700

    Version

--- NugetEnv.cmd
+++ NugetEnv.cmd
@@ -1,12 +1,8 @@
 
 powershell "$Env:nugetAPIKEY = '"APIKEY'"
-
-powershell "$Env:DrNetVersion = '6.0.0-preview.7.'+(Get-Date).ToString('yyMMdd.HHmm')"
 powershell "$Env:DrNetCommit = 'COMMIT'"
 
 powershell "[Environment]::SetEnvironmentVariable('nugetAPIKEY', 'APIKEY', 'User')"
-
-powershell "[Environment]::SetEnvironmentVariable('DrNetVersion', '6.0.0-preview.7.'+(Get-Date).ToString('yyMMdd.HHmm'), 'User')"
 powershell "[Environment]::SetEnvironmentVariable('DrNetCommit', 'COMMIT', 'User')"
 
 dotnet nuget push NUPKG --api-key %nugetAPIKEY% --source https://api.nuget.org/v3/index.json
