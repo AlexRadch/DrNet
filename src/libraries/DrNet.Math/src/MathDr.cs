ea67cc099b5f5dca273d160127a5d5eff7d10325
Alexander Radchenko <radchenkosasha@gmail.com>
2021-09-04 10:21:07 +0700

    Add DrNet.Math

---
+++ src/libraries/DrNet.Math/src/MathDr.cs
@@ -0,0 +1,92 @@
+﻿#nullable enable
+
+namespace System
+{
+    /// <summary>
+    /// TODO: MathDr
+    /// </summary>
+    public static class MathDr
+    {
+        #region IntPower
+
+        /// <summary>
+        /// TODO: IntPower
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="exp"></param>
+        /// <returns></returns>
+        public static long IntPower(long x, uint exp)
+        {
+            long result = 1;
+            while (exp > 0)
+            {
+                if ((exp & 1) == 1)
+                    result *= x;
+                exp >>= 1;
+                x *= x;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// TODO: IntPower
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="exp"></param>
+        /// <returns></returns>
+        public static ulong IntPower(ulong x, uint exp)
+        {
+            ulong result = 1;
+            while (exp > 0)
+            {
+                if ((exp & 1) == 1)
+                    result *= x;
+                exp >>= 1;
+                x *= x;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// TODO: IntPower
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="exp"></param>
+        /// <returns></returns>
+        public static long IntPowerChecked(long x, uint exp)
+        {
+            long result = 1;
+            while (exp > 0)
+            {
+                if ((exp & 1) == 1)
+                    checked { result *= x; }
+                exp >>= 1;
+                if (exp > 0)
+                    checked { x *= x; }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// TODO: IntPower
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="exp"></param>
+        /// <returns></returns>
+        public static ulong IntPowerChecked(ulong x, uint exp)
+        {
+            ulong result = 1;
+            while (exp > 0)
+            {
+                if ((exp & 1) == 1)
+                    checked { result *= x; }
+                exp >>= 1;
+                if (exp > 0)
+                    checked { x *= x; }
+            }
+            return result;
+        }
+
+        #endregion
+    }
+}
---
+++ src/libraries/DrNet.Math/tests/MathDrTests.cs
@@ -0,0 +1,50 @@
+using System;
+using Xunit;
+
+namespace DrNet.Math.Tests
+{
+    public class MathDrTests
+    {
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(0, 1)]
+        [InlineData(0, 2)]
+        [InlineData(0, 23)]
+        [InlineData(0, uint.MaxValue)]
+        [InlineData(1, 0)]
+        [InlineData(1, 1)]
+        [InlineData(1, 2)]
+        [InlineData(1, 25)]
+        [InlineData(1, uint.MaxValue)]
+        [InlineData(-1, 0)]
+        [InlineData(-1, 1)]
+        [InlineData(-1, 2)]
+        [InlineData(-1, 25)]
+        [InlineData(-1, uint.MaxValue)]
+        [InlineData(2, 0)]
+        [InlineData(2, 1)]
+        [InlineData(2, 2)]
+        [InlineData(2, 17)]
+        [InlineData(2, 62)]
+        [InlineData(-2, 0)]
+        [InlineData(-2, 1)]
+        [InlineData(-2, 2)]
+        [InlineData(-2, 14)]
+        [InlineData(-2, 17)]
+        [InlineData(-2, 63)]
+        [InlineData(17, 0)]
+        [InlineData(17, 1)]
+        [InlineData(17, 2)]
+        [InlineData(17, 7)]
+        [InlineData(-15, 0)]
+        [InlineData(-15, 1)]
+        [InlineData(-15, 2)]
+        [InlineData(-15, 7)]
+        [InlineData(int.MaxValue, 0)]
+        [InlineData(int.MaxValue, 1)]
+        [InlineData(int.MinValue, 0)]
+        [InlineData(int.MinValue, 1)]
+        public void IntPowerLong(long x, uint exp)
+            => Assert.Equal(System.Math.Pow(x, exp), MathDr.IntPower(x, exp));
+    }
+}
