ee2e59afea9793bfaf7394dc040ae5733766b250
Alexander Radchenko <radchenkosasha@gmail.com>
2021-09-04 12:54:20 +0700

    TargetFrameworks

--- src/libraries/DrNet.Extensions.String/src/StringExtensions.cs
+++ src/libraries/DrNet.Extensions.String/src/StringExtensions.cs
@@ -11,19 +11,80 @@ namespace DrNet.Extensions.String
 {
     public static class StringExtensions
     {
-        #region string.
+        #region Fast static methods access
 
         public static int Compare(this string? strA, int indexA, string? strB, int indexB, int length)
             => string.Compare(strA, indexA, strB, indexB, length);
 
         public static int Compare(this string? strA, int indexA, string? strB, int indexB, int length, bool ignoreCase)
+#if !StringNoCompareCulture
             => string.Compare(strA, indexA, strB, indexB, length, ignoreCase);
+#else
+        {
+            // Ideally we would just forward to the string.Compare overload that takes
+            // a StringComparison parameter, and just pass in CurrentCulture/CurrentCultureIgnoreCase.
+            // That function will return early if an optimization can be applied, e.g. if
+            // (object)strA == strB && indexA == indexB then it will return 0 straightaway.
+            // There are a couple of subtle behavior differences that prevent us from doing so
+            // however:
+            // - string.Compare(null, -1, null, -1, -1, StringComparison.CurrentCulture) works
+            //   since that method also returns early for nulls before validation. It shouldn't
+            //   for this overload.
+            // - Since we originally forwarded to CompareInfo.Compare for all of the argument
+            //   validation logic, the ArgumentOutOfRangeExceptions thrown will contain different
+            //   parameter names.
+            // Therefore, we have to duplicate some of the logic here.
+
+            int lengthA = length;
+            int lengthB = length;
+
+            if (strA != null)
+            {
+                lengthA = Math.Min(lengthA, strA.Length - indexA);
+            }
+
+            if (strB != null)
+            {
+                lengthB = Math.Min(lengthB, strB.Length - indexB);
+            }
+
+            CompareOptions options = ignoreCase ? CompareOptions.IgnoreCase : CompareOptions.None;
+            return CultureInfo.CurrentCulture.CompareInfo.Compare(strA, indexA, lengthA, strB, indexB, lengthB, options);
+        }
+#endif
 
         public static int Compare(this string? strA, int indexA, string? strB, int indexB, int length, bool ignoreCase, CultureInfo? culture)
+#if !StringNoCompareCulture
             => string.Compare(strA, indexA, strB, indexB, length, ignoreCase, culture);
+#else
+        {
+            CompareOptions options = ignoreCase ? CompareOptions.IgnoreCase : CompareOptions.None;
+            return Compare(strA, indexA, strB, indexB, length, culture, options);
+        }
+#endif
 
         public static int Compare(this string? strA, int indexA, string? strB, int indexB, int length, CultureInfo? culture, CompareOptions options)
+#if !StringNoCompareCulture
             => string.Compare(strA, indexA, strB, indexB, length, culture, options);
+#else
+        {
+            CultureInfo compareCulture = culture ?? CultureInfo.CurrentCulture;
+            int lengthA = length;
+            int lengthB = length;
+
+            if (strA != null)
+            {
+                lengthA = Math.Min(lengthA, strA.Length - indexA);
+            }
+
+            if (strB != null)
+            {
+                lengthB = Math.Min(lengthB, strB.Length - indexB);
+            }
+
+            return compareCulture.CompareInfo.Compare(strA, indexA, lengthA, strB, indexB, lengthB, options);
+        }
+#endif
 
         public static int Compare(this string? strA, int indexA, string? strB, int indexB, int length, StringComparison comparisonType)
             => string.Compare(strA, indexA, strB, indexB, length, comparisonType);
@@ -32,13 +93,34 @@ namespace DrNet.Extensions.String
             => string.Compare(strA, strB);
 
         public static int Compare(this string? strA, string? strB, bool ignoreCase)
+#if !StringNoCompareCulture
             => string.Compare(strA, strB, ignoreCase);
+#else
+        {
+            StringComparison comparisonType = ignoreCase ? StringComparison.CurrentCultureIgnoreCase : StringComparison.CurrentCulture;
+            return Compare(strA, strB, comparisonType);
+        }
+#endif
 
         public static int Compare(this string? strA, string? strB, bool ignoreCase, CultureInfo? culture)
-            => string.Compare(strA, strB, ignoreCase, culture);
+#if !StringNoCompareCulture
+        => string.Compare(strA, strB, ignoreCase, culture);
+#else
+        {
+            CompareOptions options = ignoreCase ? CompareOptions.IgnoreCase : CompareOptions.None;
+            return Compare(strA, strB, culture, options);
+        }
+#endif
 
         public static int Compare(this string? strA, string? strB, CultureInfo? culture, CompareOptions options)
+#if !StringNoCompareCulture
             => string.Compare(strA, strB, culture, options);
+#else
+        {
+            CultureInfo compareCulture = culture ?? CultureInfo.CurrentCulture;
+            return compareCulture.CompareInfo.Compare(strA, strB, options);
+        }
+#endif
 
         public static int Compare(this string? strA, string? strB, StringComparison comparisonType)
             => string.Compare(strA, strB, comparisonType);
@@ -76,11 +158,15 @@ namespace DrNet.Extensions.String
         public static string Format(this string format, params object?[] args)
             => string.Format(format, args);
 
+#if !StringNoIntern
         public static string Intern(this string str)
             => string.Intern(str);
+#endif
 
+#if !StringNoIntern
         public static string? IsInterned(this string str)
             => string.IsInterned(str);
+#endif
 
         //public static bool IsNullOrEmpty([NotNullWhen(false)] this string? value)
         public static bool IsNullOrEmpty(this string? value)
@@ -90,40 +176,86 @@ namespace DrNet.Extensions.String
         public static bool IsNullOrWhiteSpace(this string? value)
             => string.IsNullOrWhiteSpace(value);
 
-        #endregion
+#endregion
 
-        #region Not
+#region Contains
+
+        public static bool Contains(this string str, char value)
+            => str.IndexOf(value) >= 0;
+
+        public static bool Contains(this string str, char value, StringComparison comparisonType)
+#if !StringNoIndexOfCharComparisonType
+            => str.IndexOf(value, comparisonType) >= 0;
+#else
+            => str.IndexOf(value.ToString(), comparisonType) >= 0;
+#endif
+
+        public static bool Contains(this string str, string value, StringComparison comparisonType)
+            => str.IndexOf(value, comparisonType) >= 0;
+
+#endregion
+
+#region StartsWith EndWith
+
+        public static bool EndsWith(this string str, char value)
+        {
+            int lastPos = str.Length - 1;
+            return ((uint)lastPos < (uint)str.Length) && str[lastPos] == value;
+        }
+
+        public static bool EndsWith(this string @this, string value, bool ignoreCase, CultureInfo? culture)
+        {
+            if (null == value)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if ((object)@this == (object)value)
+            {
+                return true;
+            }
+
+            CultureInfo referenceCulture = culture ?? CultureInfo.CurrentCulture;
+            return referenceCulture.CompareInfo.IsSuffix(@this, value, ignoreCase ? CompareOptions.IgnoreCase : CompareOptions.None);
+        }
+
+        public static bool StartsWith(this string str, char value)
+             => str.Length != 0 && str[0] == value;
+
+        public static bool StartsWith(this string @this, string value, bool ignoreCase, CultureInfo? culture)
+        {
+            if (null == value)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if ((object)@this == (object)value)
+            {
+                return true;
+            }
+
+            CultureInfo referenceCulture = culture ?? CultureInfo.CurrentCulture;
+            return referenceCulture.CompareInfo.IsPrefix(@this, value, ignoreCase ? CompareOptions.IgnoreCase : CompareOptions.None);
+        }
+
+#endregion
+
+#region Not
 
         public static bool NotContains(this string str, char value)
-#if StringSearchChar
             => !str.Contains(value);
-#else
-            => str.IndexOf(value) < 0;
-#endif
 
         public static bool NotContains(this string str, char value, StringComparison comparisonType)
-#if StringSearchChar
             => !str.Contains(value, comparisonType);
-#else
-            => str.IndexOf(value.ToString(), comparisonType) < 0;
-#endif
 
         public static bool NotContains(this string str, string value)
             => !str.Contains(value);
 
         public static bool NotContains(this string str, string value, StringComparison comparisonType)
-#if StringSearchChar
             => !str.Contains(value, comparisonType);
-#else
-            => str.IndexOf(value, comparisonType) < 0;
-#endif
 
         public static bool NotEndsWith(this string str, char value)
-#if StringSearchChar
             => !str.EndsWith(value);
-#else
-            => !str.EndsWith(value.ToString());
-#endif
 
         public static bool NotEndsWith(this string str, string value)
             => !str.EndsWith(value);
@@ -140,11 +272,15 @@ namespace DrNet.Extensions.String
         public static bool NotEquals(this string? a, string? b, StringComparison comparisonType)
             => !string.Equals(a, b, comparisonType);
 
+#if !StringNoNormalization
         public static bool IsNotNormalized(this string str)
             => !str.IsNormalized();
+#endif
 
+#if !StringNoNormalization
         public static bool IsNotNormalized(this string str, NormalizationForm normalizationForm)
             => !str.IsNormalized(normalizationForm);
+#endif
 
         //public static bool IsNotNullAndNotEmpty([NotNullWhen(false)] this string? value)
         public static bool IsNotNullAndNotEmpty(this string? value)
@@ -155,11 +291,7 @@ namespace DrNet.Extensions.String
             => !string.IsNullOrWhiteSpace(value);
 
         public static bool NotStartsWith(this string str, char value)
-#if StringSearchChar
             => !str.StartsWith(value);
-#else
-            => !str.StartsWith(value.ToString());
-#endif
 
         public static bool NotStartsWith(this string str, string value)
             => !str.StartsWith(value);
